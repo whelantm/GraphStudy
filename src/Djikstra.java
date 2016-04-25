7d6f3a9f37028f0bf3f61f2e1e3b084954edd3ec
whelantm <travis.m.whelan@gmail.com>
2016-04-24 20:21:17 -0500

    starting djikstra

--- src/Djikstra.java
+++ src/Djikstra.java
@@ -7,8 +7,10 @@ public class Djikstra {
     private Graph graph;
     private List<Edge> edges;
     private List<Node> nodes;
-    private Set<Node> settledEdges;
-    private Set<Node> unsettledEdges;
+    private Set<Node> settledNodes;
+    private Set<Node> unsettledNodes;
+    private Map<Node, Integer> distances;
+    private Map<Edge, Edge> predecessor;
 
     public Djikstra(Graph graph) {
         this.graph = graph;
@@ -17,4 +19,22 @@ public class Djikstra {
     }
 
 
+    public int[] search(int source) {
+        settledNodes = new HashSet<>();
+        unsettledNodes = new HashSet<>();
+        distances = new HashMap<>();
+        predecessor = new HashMap<>();
+        Node start;
+        for (Node n : nodes) {
+            if (n.data == source) {
+                start = n;
+                distances.put(n, 0);
+            }
+            else {
+                distances.put(n, Integer.MAX_VALUE);
+            }
+        }
+
+        return new int[0];
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -13,22 +13,26 @@ public class Program {
             int n = s.nextInt();
             int e = s.nextInt();
 
-            Node[] nodes = new Node[n];
-            ArrayList<Edge> edges = new ArrayList<Edge>();
+            Graph graph = new Graph();
 
-            for(int i = 0; i < n; i++) {
-                nodes[i] = new Node(i + 1);
+            for(int i = 1; i <= n; i++) {
+                graph.addNode(i);
             }
 
             for(int i = 0; i < e; i ++) {
                 int n1 = s.nextInt();
                 int n2 = s.nextInt();
                 int w = s.nextInt();
-                Edge edge = new Edge(nodes[n1-1], nodes[n2-1], w);
-                nodes[n1 -1].addEdge(edge);
-                edges.add(edge);
-                nodes[n1 - 1].edges.add(edge);
+                try {
+                    graph.createEdge(n1, n2, w);
+                }
+                catch (Exception ex) {
+
+                }
             }
+
+            Djikstra djikstra = new Djikstra(graph);
+            int[] result = djikstra.search(1);
         }
     }
 }
